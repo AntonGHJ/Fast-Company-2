04bee5950cbd01f2a1926b214872945ddd608fce
AntonAN <anton89andreev@gmail.com>
2022-10-05 10:31:42 +0200

    Sorting

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,21 +1,45 @@
 import { noConflict } from "lodash";
 import React, { useState, useEffect } from "react";
 import Pagination from "./pagination";
-import User from "./user";
 import { paginate } from "../utils/paginate";
 import PropTypes from 'prop-types';
 import GroupList from "./groupList";
 import api from "../api";
 import SearchStatus from './searchStatus'
+import UserTable from "./usersTable";
+import _ from 'lodash'
 
-const Users = ({ users, ...rest }) => {
+const Users = () => {
        
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions]=useState([])
     const [selectedProf, setSelectedProf]=useState()
+    const [sortBy, setSortBy]=useState({iter:'name', order:'asc'})
     
-    const pageSize = 2;
-
+    const pageSize = 8;
+    
+    const [users, setUsers] = useState([]);
+        
+        useEffect(()=>{
+            api.users.fetchAll().then((data)=>{
+                setUsers(data)
+            })
+        }, [])    
+       
+        const handleDelete = (userId) => {
+            setUsers(users.filter((user) => user._id !== userId));
+        };
+        const handleToggleBookMark = (id) => {
+            setUsers(
+                users.map((user) => {
+                    if (user._id === id) {
+                        return { ...user, bookmark: !user.bookmark };
+                    }
+                    return user;
+                })
+            );
+        };
+    
     useEffect(() => {       
        api.professions.fetchAll().then((data)=>setProfessions(data))                
     }, [])
@@ -31,12 +55,22 @@ const Users = ({ users, ...rest }) => {
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
     };
+
+    const handleSort=(item)=>{
+        setSortBy(item)
+    }
+
+    if(users){
+
     const filteredUsers = selectedProf? users.filter(
         (user) => JSON.stringify(user.profession)===JSON.stringify(selectedProf)
             ): users;
     
     const count = filteredUsers.length;
-    const userCrop = paginate(filteredUsers, currentPage, pageSize);
+    
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.path], [sortBy.order])
+ 
+    const userCrop = paginate(sortedUsers, currentPage, pageSize);
     const clearFilter = () =>{setSelectedProf()}
 
     return (        
@@ -55,24 +89,13 @@ const Users = ({ users, ...rest }) => {
         <div className="d-flex flex-column">
           <SearchStatus length={count} />
             {count > 0 && (              
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Провфессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th scope="col">Избранное</th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {userCrop.map((user) => (
-                            <User key={user._id} {...rest} {...user} />
-                        ))}
-                    </tbody>
-                </table>
+                <UserTable 
+                users={userCrop} 
+                onDelete={handleDelete}
+                onToggleBookMark={handleToggleBookMark}
+                onSort={handleSort}
+                selectedSort={sortBy}
+                />
                
             )}
             <div className="d-flex justify-content-center">
@@ -86,7 +109,8 @@ const Users = ({ users, ...rest }) => {
         </div>           
     </div>
       
-    );
+    );}
+    return 'loading'
 };
 
 Users.propTypes = {
@@ -95,4 +119,4 @@ Users.propTypes = {
     
 };
 
-export default Users;
+export default Users
